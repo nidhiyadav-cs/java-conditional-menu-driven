7c1e27b35865f85b2f2e835f2eec1c1ef8ad6ccc
nidhiyadav-cs <nidhiyadav10y@gmail.com>
2026-09-13 21:22:57 +0530

    Add Greater of three numbere

---
+++ Greater.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Greater
+{
+    public static void main(String[] args) 
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int a = scanner.nextInt();
+        System.out.print("Enter second number: ");
+        int b = scanner.nextInt();
+        System.out.print("Enter third number: ");
+        int c = scanner.nextInt();
+        if (a > b && a > c) 
+        {
+            System.out.println("The greatest number is: " + a);
+        } 
+        else if (b > a && b > c) 
+        {
+            System.out.println("The greatest number is: " + b);
+        } 
+        else 
+        {
+            System.out.println("The greatest number is: " + c); 
+        }
+    }
+}
